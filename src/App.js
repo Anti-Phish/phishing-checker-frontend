394a0ac48d35356daa349340879e671c0c6571ab
Wikum Chamith <wikum@linuxdeveloper.space>
2021-08-19 10:33:51 +0530

    Added a server health check

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import Logo from './assets/logo.png';
 import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Home from './components/Home';
+import axios from "axios";
 
+
+setInterval(HealthCheck, 60000)
 
 function App() {
   return (
@@ -10,13 +13,16 @@ function App() {
       <div className="container">
         <img src={Logo} className="rounded mx-auto d-block" style={{ height: 150 }} alt="logo" id="img1" />
         {/* <img src={Solong} className="rounded float-right" alt="solong" id="img2"/> */}
-
         <Home/>
-
-        
       </div>
     </Router>
   );
+}
+
+function HealthCheck () {
+    axios.get("https://anti-phish-frontend.herokuapp.com/").then((response) => {
+        console.log("Health ok")
+    })
 }
 
 export default App;
