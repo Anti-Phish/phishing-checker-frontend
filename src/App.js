e072fa1b5c296249d4ef93452c88ff8d3f51181d
Wikum Chamith <wikum@linuxdeveloper.space>
2021-09-03 16:15:24 +0530

    Removed duplicate public folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './components/Home';
 import Feedback from './components/Feedback';
-import {toast, ToastContainer} from "react-toastify";
 import React from "react";
 import Switch from "react-bootstrap/Switch";
 import axios from "axios";
@@ -18,13 +17,10 @@ function App() {
     <Router>
       <div style={{marginBottom:"0px"}}>
         <img src={Logo} className="rounded mx-auto d-block" style={{ height: 150 }} alt="logo" id="img1" />
-
-
         <Switch>
             <Route path="/" exact component={Home}/>
             <Route path="/feedback" component={Feedback}/>
         </Switch>
-
       </div>
      </Router>
   );
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css'
-import Switch from "react-bootstrap/Switch";
-import {BrowserRouter as Router,Route} from "react-router-dom";
 
 
 
