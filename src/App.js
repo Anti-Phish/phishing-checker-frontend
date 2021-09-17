0239ee376a484df34f43e4d0cb8ab528233f4571
kavishka ganewattha <kaviya1234z@gmail.com>
2021-09-17 21:41:05 +0530

    New designed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Logo from './assets/logo.png';
-// import './styls/App.css';
+import Logo from './assets/mainLogo.png';
 import { BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './components/Home';
 import Feedback from './components/Feedback';
 import Footer from './components/Footer';
 import React from "react";
-import Switch from "react-bootstrap/Switch";
+import AboutUs from "./components/AboutUs";
 
 function App() {
 
@@ -13,10 +12,11 @@ function App() {
     <Router>
       <div style={{marginBottom:"0px"}}>
         <img src={Logo} className="rounded mx-auto d-block" style={{ height: 150 }} alt="logo" id="img1" />
-        <Switch >
+
             <Route path="/" exact component={Home}/>
             <Route path="/feedback" component={Feedback}/>
-        </Switch>
+            <Route path="/about" component={AboutUs}/>
+
           <Footer />
       </div>
      </Router>
